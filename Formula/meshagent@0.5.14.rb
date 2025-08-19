7ff1b4b5def39980b3030c13ba70b3ab98180a2a
Brandon Bethke <brandon.bethke@timu.com>
2025-08-19 05:41:03 +0000

    updated formula

--- Formula/meshagent@0.5.14.rb
+++ Formula/meshagent@0.5.14.rb
@@ -5,7 +5,7 @@ class MeshagentAT0514 < Formula
 
   on_macos do
     url "https://storage.googleapis.com/meshagent-cli-builds/0.5.14/meshagent-0.5.14-macos.tar.gz"
-    sha256 "4e344aa28c7dccdbf007b61fcb371e6a5766529de4c1fde356fc19cb4ba97d85"
+    sha256 "49f4102ada3ebdb1f66e0f8a5b82cc1a328e078db7aca9c5f1b19ed65f07e6b6"
   end  
 
   def install
